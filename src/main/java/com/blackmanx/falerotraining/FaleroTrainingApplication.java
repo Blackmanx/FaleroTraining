673101704b3f20a6dc3550174b93fb11a2b2b31c
Blackmanx <blackman@sajl.tk>
2024-03-05 20:09:06 +0100

    Initialize project
    * Initialized with Spring Initializer https://start.spring.io/
    * Used Java 21, Maven
    * Dependencies: Data JPA, mySQL Driver, Spring Web, Spring DevTools, Thymeleaf

---
+++ src/main/java/com/blackmanx/falerotraining/FaleroTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.blackmanx.falerotraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaleroTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaleroTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackmanx/falerotraining/FaleroTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackmanx.falerotraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaleroTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
